8dc08850d28afd25229301b5d431778572c7391b
DuyChau <duy.chau@gleads.vn>
2024-12-26 15:26:44 +0700

    debug

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -15,8 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
-const auth = getAuth(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 
 export default app;
-export { auth, db };
