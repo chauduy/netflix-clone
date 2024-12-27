8710f06f4dec8787ff00c50259e9205865f4210a
DuyChau <duy.chau@gleads.vn>
2024-12-27 10:34:24 +0700

    debug

--- src/hook/useList.tsx
+++ src/hook/useList.tsx
@@ -12,23 +12,23 @@ import { useEffect, useState } from "react";
 function useList(uid: string | undefined) {
     const [list, setList] = useState<Movie[] | DocumentData[] | null>([]);
 
-    useEffect(() => {
-        if (!uid) return;
+    // useEffect(() => {
+    //     if (!uid) return;
 
-        if (db && db instanceof Firestore) {
-            return onSnapshot(
-                collection(db, "customers", uid, "myList"),
-                (snapshot) => {
-                    setList(
-                        snapshot.docs.map((doc) => ({
-                            id: doc.id,
-                            ...doc.data(),
-                        }))
-                    );
-                }
-            );
-        }
-    }, [db, uid]);
+    //     if (db && db instanceof Firestore) {
+    //         return onSnapshot(
+    //             collection(db, "customers", uid, "myList"),
+    //             (snapshot) => {
+    //                 setList(
+    //                     snapshot.docs.map((doc) => ({
+    //                         id: doc.id,
+    //                         ...doc.data(),
+    //                     }))
+    //                 );
+    //             }
+    //         );
+    //     }
+    // }, [db, uid]);
 
     return list;
 }
