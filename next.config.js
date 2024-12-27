1d50e5cb7976e71b27a60b399bea4b2127855c5a
DuyChau <duy.chau@gleads.vn>
2024-12-27 14:42:27 +0700

    unoptimize image

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     },
     images: {
         domains: ["image.tmdb.org", "rb.gy", "themoviedb.org"],
+        unoptimized: true,
     },
     transpilePackages: ["@stripe/firestore-stripe-payments"],
 };
